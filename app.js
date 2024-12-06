f80104f61beb10dc5aa78bf4f55b2106fcba7376
arizvi002 <arizvi.1993@gmail.com>
2024-12-06 13:01:06 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is a Node.js app running in a container! Checking if webhook is working 2');
+    res.send('Hello, World! This is a Node.js app running in a container! Checking if webhook is working again');
 });
 
 const PORT = process.env.PORT || 3000;
